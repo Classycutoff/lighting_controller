b7e6e1363cae8512fbb8961687aa7c2ab21454d8
AA3737 <AA3737@student.jamk.fi>
2024-02-13 19:24:15 +0200

    I don't know why, but the pipeline ignore the first write and every second one after that. Will have to look for the reason.

--- main.py
+++ main.py
@@ -17,12 +17,13 @@ try:
                 # dmxClient.write_DMX(
                 #     {dmx_channel + 1: 255, dmx_channel + 1: 255}
                 # )  # DMX is counted from 1 upwards, and that's why it is + 1.
-                dmx_dict = ef_all_on()
+                # dmx_dict = ef_all_on()
 
                 # print(dmx_dict)
-                # dmx_dict = ef_rotate(dmx_dict, 24)
+                dmx_dict = ef_rotate(dmx_dict, 24)
                 print(dmx_dict)
 
+                dmxClient.write_DMX(dmx_dict)
                 dmxClient.write_DMX(dmx_dict)
 
                 time.sleep(0.04)  # Added so that KeyBoardInterrupt has time to work.
